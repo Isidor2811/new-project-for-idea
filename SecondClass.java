6e0b66ebd6d6ceed7985493214cde3cbb68b96ee
Jack <automation1@test.com>
2023-05-06 19:35:03 +0300

    third commit

--- SecondClass.java
+++ SecondClass.java
@@ -1,7 +1,7 @@
 public class SecondClass {
 
     public static void main(String[] args) {
-        System.out.println("Bye");
+        System.out.println("Bye-bye");
     }
 
 }
--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,10 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.print("Hello!!!");
+    }
+
+    public static void test(){
+        System.out.println("123");
     }
 }
