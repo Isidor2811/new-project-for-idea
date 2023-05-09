e2b0c1238a98e169e44054b95b74c6925f9925e9
Jack <automation1@test.com>
2023-05-09 20:48:34 +0300

    refactored test for fix

--- Animal.java
+++ Animal.java
@@ -2,5 +2,6 @@ public class Animal {
 
     public String name;
     public int age;
+    public double weight;
 
 }
--- SecondClass.java
+++ SecondClass.java
@@ -2,6 +2,7 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println("Bye-bye");
+        System.out.println("Bye-bye");
     }
 
 }
