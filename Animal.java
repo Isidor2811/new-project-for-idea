1c35529eb3a1faa8710751930c7d130905870a87
Jack <automation1@test.com>
2023-05-12 06:13:32 +0300

    fixed tests from feature 836

--- Animal.java
+++ Animal.java
@@ -1,8 +1,15 @@
 public class Animal {
 
-    public String name;
+    public String nname;
     public int age;
     public double weight;
     public double height;
 
+    public void feedAnimal(){
+        System.out.println("Feeding...");
+    }
+
+    public void GoForAWalk(){
+        System.out.println("walking...");
+    }
 }
--- TestClass.java
+++ TestClass.java
@@ -11,13 +11,16 @@ public class TestClass {
     
     public void bestMethod13(){
         //some code here
+        System.out.println(1);
     }
 
     public void newMethod11(){
         //some code here
+        System.out.println(2);
     }
 
     public void bestMethod15(){
         //some code here
+        System.out.println(4);
     }
 }
